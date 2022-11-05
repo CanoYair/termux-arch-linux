d0d97ee375ce657b8649cbf5bde653e62868157c
robot95 <104598388+Robot9554tt@users.noreply.github.com>
2022-11-04 21:20:04 -0930

    Bash Script Termux Arch Linux
    Termux bash Termux INtalacion y actuliazacion y instalacion de propiedades y herramientas para la basica implemntacion

---
+++ termux-bash.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+
+
+pkg update && pkg upgrade -y
+pkg update
+pkg upgrade
+
+pkg update
+pkg upgrade
+pkg install root-repo
+pkg install unstable-repo
+pkg install x11-repo
+pkg install git
+pkg install coreutils
+pkg install wget nano tmux zsh neofetch curl
+
+
+
+pkg update -y
+pkg install 
+pkg install git 
+pkg install wget 
+pkg install curl 
+pkg install bsdtar -y
+
+
+pkg update && pkg upgrade -y
+pkg update
+pkg upgrade
+
+
+
+pkg install -y python
+
+pkg install -y python2
+
+pkg install python3
+
+pkg install -y git
+
+
+
+git clone https://github.com/TermuxArch/TermuxArch.git
+
+
+
+sudo chmod 777 -R TermuxArch
+
+cd TermuxArch
+
+sudo ./TermuxArch
+
+bash setupTermuxArch
+
+
+
+pkg update && pkg upgrade -y
+pkg update
+pkg upgrade
+
+pkg update
+pkg upgrade
